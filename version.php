3745c078beb7f2a6f5d3016bda5c79f3f0705e53
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-16 13:06:02 -0300

    V 1.1.1

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2026091400;
-$plugin->release = '1.1.0';
+$plugin->version = 2026091600;
+$plugin->release = '1.1.1';
 $plugin->component = "mod_audioanswer";
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
